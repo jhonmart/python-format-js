d672fedc41060a460ace904cfc82d61aecb94fc1
Rômulo Rocha de Oliveira <romulorocha063@gmail.com>
2019-06-29 14:52:32 -0700

    Desenvolvimento (#3)
    * Melhoria nos parametros e adição de funções inicias
    
    * Atualização de versão para a atual
    
    * Upando Readme + Contribuiting
    
    * Adicionando testes
    
    * Correção de variável errada
    
    * Atualização versão
    
    * Adicionando gitignore
    
    * Adição de exemplos

--- index.js
+++ index.js
@@ -1,36 +1,40 @@
 Object.defineProperty(String.prototype, 'format', {
-	value: function(...str) {
+
+	value: function (...str) {
 		let i = 0,
 			e = this; // Entrada
-			
-		e.split(' ').map(nth=>{
-			if(e.includes('{}'))
-				e = e.replace('{}', str.splice(0,1)[0])
+
+		e.split(' ').map(nth => {
+			if (e.includes('{}'))
+				e = e.replace('{}', str.splice(0, 1)[0])
 		});
 
-		return !e.includes('{')? e : e.split(/([{}])/g).filter(a=>!['{','}'].includes(a)).map(c=>{
-			let params = +c.slice(1) && !c.includes('.')? +c.slice(1) : /:(\D)+?(\D)*?(\d.*)/g.exec(c) || c, // /:(\D)+?(\D)*?(\d.*)*?(\D)+?/g
-				str_a = str[i];
+		return !e.includes('{') ? e : e.split(/([{}])/g).filter(a => !['{', '}'].includes(a)).map(c => {
+			let params = +c.slice(1) && !c.includes('.') ? +c.slice(1) : /:(\D)+?(\D)*?(\d.*)/g.exec(c) || c; // /:(\D)+?(\D)*?(\d.*)*?(\D)+?/g
 
-			if(typeof params=="string") return c;
-			else if(typeof params=="number") return " ".repeat(params).replace(RegExp(`.{${str_a.length}}`), str[i++]);
-			else{
-				let p1 = params[1],
-					fill_elem = ([...'<^>.'].includes(p1)? ' ':p1),
-					space = !p1.includes('.')? fill_elem.repeat((+params[3])-str_a.length) : '',
-					elem = [space, str_a],
-					srt_crop = str_a.slice(0,+params[3]),
-					space_c = fill_elem.repeat(+params[3]),
-					num = Math.floor((space_c.length-str_a.length)/2),
-					str_pos = num>0? fill_elem.repeat(num)+str[i++]: '',
-					elem_center = space_c.replace(RegExp(`.{${str_pos.length}}`), str_pos);
+			if (typeof params == "string") return c;
+			else {
+				if (typeof params == "number") return " ".repeat(params).replace(RegExp(`.{${str[i].length}}`), str[i]);
+				else {
+					let p1 = params[1],
+						fill_elem = ([...'<^>.'].includes(p1) ? ' ' : p1),
+						space = !p1.includes('.') ? fill_elem.repeat((+params[3]) - str[i].length) : '',
+						elem = [space, str[i]],
+						srt_crop = str[i].slice(0, +params[3]),
+						space_c = fill_elem.repeat(+params[3]),
+						num = Math.floor((space_c.length - str[i].length) / 2),
+						str_pos = num > 0 ? fill_elem.repeat(num) + str[i] : '',
+						elem_center = space_c.replace(RegExp(`.{${str_pos.length}}`), str_pos);
 
-				return (params.includes('>')?  elem : 
-						params.includes('<')? elem.reverse() : 
-						params.includes('^')? [elem_center] :  
-						params.includes('.')? [srt_crop] : '').join('');
+					i++;
+					return (params.includes('>') ? elem :
+						params.includes('<') ? elem.reverse() :
+							params.includes('^') ? [elem_center] :
+								params.includes('.') ? [srt_crop] : '').join('');
+				}
+
 			}
-			
+
 		}).join('');
 	}
 });
---
+++ tests.test.js
@@ -0,0 +1,22 @@
+require('./index')
+
+
+test('Formatação de alinhamento', () => {
+  expect("{:<6} {:<28} {:>5} {:^9}".format("abs", "x  1 teste", "x2", "x3"))
+    .toEqual('abs    x  1 teste                      x2    x3    ')
+})
+
+test('Formatação de alinhamento 2', () => {
+  expect("{:<6} {:^28} {:>5} {:^9}".format("abs", "x  1 teste", "x2", "x3"))
+    .toEqual('abs             x  1 teste             x2    x3    ')
+})
+
+test("Formatação de alinhamento com excesso de caracteres", () => {
+  expect("{:<6} {:<28} {:>5} {:^9}".format("absssss", "x  1 teste", "x2", "x3"))
+    .toEqual('absssss x  1 teste                      x2    x3    ')
+})
+
+test("Formatação de alinhamento com mais paramentros: '_'", () => {
+  expect("{:_<6} {:<28} {:>5} {:^9}".format("abs", "x  1 teste", "x2", "x3"))
+    .toEqual('abs___ x  1 teste                      x2    x3    ')
+})
