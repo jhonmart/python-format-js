bb42e3e526b5266422950225cc54bdcbd130e21d
Jonatas Martins <jonatas777.gato@gmail.com>
2019-06-30 15:34:46 -0300

    Melhorias gerais, criação inicial de falhas e melhoria na passagem dos parametros

--- index.js
+++ index.js
@@ -1,38 +1,73 @@
+"use strict"
 Object.defineProperty(String.prototype, 'format', {
 	value: function(...str) {
 		let i = 0,
-			e = this; // Entrada
-			
-		e.split(' ').map(nth=>{
-			if(e.includes('{}'))
-				e = e.replace('{}', str.splice(0,1)[0])
-		});
+			e = this, // Entrada
+			removeStr = [],
+			srtSpc = e.match(/({+.*?})/g);
 
-		return !e.includes('{')? e : e.split(/([{}])/g).filter(a=>!['{','}'].includes(a)).map(c=>{
-			let params = +c.slice(1) && !c.includes('.')? +c.slice(1) : /:(\D)+?(\D)*?(\d.*)/g.exec(c) || c; // /:(\D)+?(\D)*?(\d.*)*?(\D)+?/g
+		str = str.map(el=>el+''); // toString
 
-			if(typeof params=="string") return c;
-			else{
-				if(typeof params=="number") return " ".repeat(params).replace(RegExp(`.{${str[i].length}}`), str[i]);
-				else{
-					let p1 = params[1],
-						fill_elem = ([...'<^>.'].includes(p1)? ' ':p1),
-						space = !p1.includes('.')? fill_elem.repeat((+params[3])-str[i].length) : '',
-						elem = [space, str[i]],
-						srt_crop = str[i].slice(0,+params[3]),
-						space_c = fill_elem.repeat(+params[3]),
-						num = Math.floor((space_c.length-str[i].length)/2),
-						str_pos = num>0? fill_elem.repeat(num)+str[i]: '',
-						elem_center = space_c.replace(RegExp(`.{${str_pos.length}}`), str_pos);
+		let paramStr = srtSpc.map(p=>{
+			let isNum = +p.replace(/[{}]/g,'');
+			return (p.match(/{(\D+.*?)/g)? [true] : [isNum<str.length])[0];
+		}).filter(e=>!e); 
+
+		let refParam = srtSpc.map(p=>{
+			let isNum = +p.replace(/[{}]/g,'');
+			return p.match(/{(\D+.*?)/g)? isNum : true;
+		}).filter(p=>p);
+		
+		if(paramStr.length)
+			return "Fail ref"; // Fail line
+		else if(srtSpc.length-refParam.length>str.length || srtSpc.length-refParam.length>=str.length && refParam.length)
+			return "Overflow of parameters greater than amount of values"; // Fail line
+		else
+			srtSpc.map((nth,pE)=>{
+				let pElem = /\d+.*?/.exec(nth);
 					
-					i++;
-					return (params.includes('>')?  elem : 
-							params.includes('<')? elem.reverse() : 
-							params.includes('^')? [elem_center] :  
-							params.includes('.')? [srt_crop] : '').join('');
+				if(nth.includes('{}')){ // Change default
+					e = e.replace(nth, str[pE]); // Change for value
+					removeStr.push(pE);
 				}
-			}
+				else if(pElem){
+					if(pElem[0].length+2==nth.length)
+						e = e.replace(nth, str[+pElem[0]]); // Change for value
+					else if(str.length>pE && !nth.includes('.')) // Overflow string lenght
+						if(str[pE].length >= +( pElem || ['0'])[0]){ 
+							e = e.replace(nth, str[pE]); // Change for value
+							removeStr.push(pE);
+						}
+				}
+			});
 			
-		}).join('');
+			removeStr.reverse().map(pE=>str.splice(pE,1));
+
+			return !e.includes('{')? e : e.split(/([{}])/g).filter(a=>!['{','}'].includes(a)).map(c=>{
+				let params = +c.slice(1) && !c.includes('.')? +c.slice(1) : /:(\D)+?(\D)*?(\d.*)/g.exec(c) || c; // /:(\D)+?(\D)*?(\d.*)*?(\D)+?/g
+
+				if(typeof params=="string") return c;
+				else{
+					if(typeof params=="number") return " ".repeat(params).replace(RegExp(`.{${str[i].length}}`), str[i]);
+					else{
+						let p1 = params[1],
+							fill_elem = ([...'<^>.'].includes(p1)? ' ':p1),
+							space = !p1.includes('.')? fill_elem.repeat((+params[3])-str[i].length) : '',
+							elem = [space, str[i]],
+							srt_crop = str[i].slice(0,+params[3]),
+							space_c = fill_elem.repeat(+params[3]),
+							num = Math.floor((space_c.length-str[i].length)/2),
+							str_pos = num>0? fill_elem.repeat(num)+str[i]: '',
+							elem_center = space_c.replace(RegExp(`.{${str_pos.length}}`), str_pos);
+						
+						i++;
+						return (params.includes('>')?  elem : 
+								params.includes('<')? elem.reverse() : 
+								params.includes('^')? [elem_center] :  
+								params.includes('.')? [srt_crop] : '').join('');
+					}
+				}
+				
+			}).join('');
 	}
 });
